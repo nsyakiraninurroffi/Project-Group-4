134a1aee050f761637efcaa6c250aff2d36d508c
= <=>
2025-10-01 12:46:31 +0700

    backend tamu

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
+use App\Http\Controllers\BukuTamuController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/buku-tamu',[BukuTamuController::class, 'index']);
+Route::post('/buku-tamu',[BukuTamuController::class, 'store'])->name('bukutamu.store');
